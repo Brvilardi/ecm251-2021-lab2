1c7a95f04381785b21946996c0aacba706ad2dfe
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-02-25 09:21:59 -0300

    retirar uma linha do helloWorld

--- Aula01/prog1/helloWorld.java
+++ Aula01/prog1/helloWorld.java
@@ -1,7 +1,6 @@
 class helloWorld{
     public static void main(String[] args) {
         System.out.println("Hello, World!! From Java!");
-        System.out.println("Hello, World!! From Java2!");
     }
 }
 
