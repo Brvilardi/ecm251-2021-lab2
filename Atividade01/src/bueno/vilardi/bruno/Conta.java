c543628316efa877410a5588a3c2849206664223
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-03-22 11:26:45 -0300

    metodos classe Conta

--- Atividade01/src/bueno/vilardi/bruno/Conta.java
+++ Atividade01/src/bueno/vilardi/bruno/Conta.java
@@ -1,5 +1,7 @@
 package bueno.vilardi.bruno;
 
+import java.util.ArrayList;
+
 public class Conta {
     public int id;
     private double saldo;
@@ -14,6 +16,29 @@ public class Conta {
         contador++;
     }
 
+    private void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    private boolean sacar(double valor){
+        if(valor <= this.saldo){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferirDinheiro(String s){
+        String[] dados = s.split(";");
+        System.out.println(dados);
+        /*
+        if (this.sacar(valor)){
+            destino.depositar(valor);
+            return true;
+        }
+        return false;*/
+    }
+
     @Override
     public static String toString() {
         return "Conta{" +
@@ -21,9 +46,6 @@ public class Conta {
                 ", saldo=" + this.saldo +
                 '}';
     }
-
-    
-
 
 }
 
